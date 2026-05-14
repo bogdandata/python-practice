3bd76d529331121a457a091e9f8d8fb8c4e9b55f
bogdan stanojevic <gobussiness4@gmail.com>
2026-05-14 13:26:43 +0200

    Format figlet.py with consistent indentation

--- figlet.py
+++ figlet.py
@@ -6,8 +6,8 @@ def main():
     f = Figlet() 
     if len(sys.argv) == 1:
         font = random.choice(f.getFonts())
-    elif len(sys.argv) == 3 and sys.argv[1] in ("-f", "--font"):#check if the user 
-                                                                #provided a font and if it's valid
+    elif len(sys.argv) == 3 and sys.argv[1] in ("-f", "--font"):
+                                                                
         font = sys.argv[2]
         if font not in f.getFonts():
             print("Invalid font")
